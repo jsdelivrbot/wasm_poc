70e57b1a8ecdca49b81953933e3e984f2d52dc98
Peter Ballok <peter.ballok@logmein.com>
2018-01-10 09:49:56 +0100

    Import React

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>Hi Mom!</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
