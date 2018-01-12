8e653ec6cc7a10d89bbebb4e38dc79da3fb2ed0c
Peter Ballok <pballok@gmail.com>
2018-01-12 16:51:11 +0100

    Use two-dimensional array of ints instead of array of strings

--- generate_tickets.py
+++ generate_tickets.py
@@ -8,14 +8,14 @@ numbers_per_ticket = 5
 for i in range(number_of_tickets):
     shuffle(numbers)
 
-    print("\"", end="")
+    print("[", end="")
 
     for j in range(numbers_per_ticket):
         print(numbers[j], end="")
         if(j != numbers_per_ticket - 1):
-            print(" ", end="")
+            print(", ", end="")
 
     if(i != number_of_tickets - 1):
-        print("\",")
+        print("],")
     else:
-        print("\"")
+        print("]")
