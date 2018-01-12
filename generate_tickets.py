0337710b4b6ef7a1e9e7659d9c42abf417a3c000
Peter Ballok <pballok@gmail.com>
2018-01-12 16:44:58 +0100

    Generate tickets with a python script

---
+++ generate_tickets.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from random import *
+
+numbers = list(range(1, 91))
+number_of_tickets = 500
+numbers_per_ticket = 5
+
+for i in range(number_of_tickets):
+    shuffle(numbers)
+
+    print("\"", end="")
+
+    for j in range(numbers_per_ticket):
+        print(numbers[j], end="")
+        if(j != numbers_per_ticket - 1):
+            print(" ", end="")
+
+    if(i != number_of_tickets - 1):
+        print("\",")
+    else:
+        print("\"")
